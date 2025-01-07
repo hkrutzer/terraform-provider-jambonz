3f670f78fd612d0acaa787a5d80fa235b489c6e0
Hans Krutzer <git@pixelspaceships.com>
2025-01-07 13:42:09 +0100

    Add documentation

---
+++ examples/resources/jambonz_phone_number/resource.tf
@@ -0,0 +1,6 @@
+resource "jambonz_phone_number" "test_num" {
+  account_sid      = "85358b52-4264-4ec4-9d76-d5c5971b1ed5"
+  phone_number     = "45111111111"
+  voip_carrier_sid = "6cb36139-47bc-4a11-882d-80af726a022a"
+  application_sid  = "7e299ed5-53e5-4964-88d8-386a495b0c0c"
+}
